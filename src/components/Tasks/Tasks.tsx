acc55b2744f23867cf6c7e97873b612034011785
Vadim Majsuk <vmajsuk@gmail.com>
2021-12-22 13:45:52 +0300

    feat: added type-safe Task component

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { RouteComponentProps as RCP } from "react-router-dom";
+
 import { fetchTasks } from "~/api";
+
+import { Task } from "../Task";
 
 interface TasksParams {
   userId: string;
@@ -20,9 +23,18 @@ export const Tasks: React.FC<RCP<TasksParams>> = ({
       {tasks.error && <div>Error: {JSON.stringify(tasks.error)}</div>}
       {tasks.data && (
         <div>
-          {tasks.data.map((task) => (
-            <div key={task.id}>{task.title}</div>
-          ))}
+          {tasks.data.map((task) =>
+            task.completed ? (
+              <Task isCompleted key={task.id} title={task.title} />
+            ) : (
+              <Task
+                key={task.id}
+                isCompleted={false}
+                title={task.title}
+                onComplete={console.log}
+              />
+            )
+          )}
         </div>
       )}
     </div>
